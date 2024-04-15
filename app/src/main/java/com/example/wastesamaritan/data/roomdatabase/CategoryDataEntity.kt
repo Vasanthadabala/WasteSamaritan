358fe92c592159de24bb8576eb9e01549e9652be
unknown <vasanthadabala@gmail.com>
2024-04-15 22:42:17 +0530

    Room Database update

--- app/src/main/java/com/example/wastesamaritan/data/roomdatabase/CategoryDataEntity.kt
+++ app/src/main/java/com/example/wastesamaritan/data/roomdatabase/CategoryDataEntity.kt
@@ -1,6 +1,7 @@
 package com.example.wastesamaritan.data.roomdatabase
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "not_segregated_data")
@@ -12,12 +13,15 @@ data class NotSegregatedDataEntity(
     val screenType: String
 )
 
-@Entity(tableName = "segregated_data")
+@Entity(tableName = "segregated_data",
+    primaryKeys = ["id", "category"],
+    indices = [Index(value = ["category"], unique = true)])
 data class SegregatedDataEntity(
-    @PrimaryKey val id: String,
+    val id: String,
     val category: String,
     val capturedImageUris: List<String>,
     val weightCards: MutableList<Double>,
     val rating: Double,
     val screenType: String
 )
+
--- app/src/main/java/com/example/wastesamaritan/data/roomdatabase/Database.kt
+++ app/src/main/java/com/example/wastesamaritan/data/roomdatabase/Database.kt
@@ -3,6 +3,8 @@ package com.example.wastesamaritan.data.roomdatabase
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(entities = [NotSegregatedDataEntity::class, SegregatedDataEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
